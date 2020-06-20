f0d09311602906a5941d5d6335a970bb3a8d53ad
zunsking <36225896+zunsking@users.noreply.github.com>
2020-06-20 13:56:09 +0800

    Update lnmp-sub.sh

--- lnmp-sub.sh
+++ lnmp-sub.sh
@@ -7,8 +7,8 @@ cd /usr/local
 git clone git://github.com/FRiCKLE/ngx_cache_purge.git
 git clone git://github.com/yaoweibin/ngx_http_substitutions_filter_module
 cd
-wget http://soft.vpser.net/lnmp/lnmp1.6.tar.gz -cO lnmp1.6.tar.gz
-tar zxf lnmp1.6.tar.gz
-cd lnmp1.6
+wget http://soft.vpser.net/lnmp/lnmp1.7.tar.gz -cO lnmp1.7.tar.gz
+tar zxf lnmp1.7.tar.gz
+cd lnmp1.7
 sed -i "s:Nginx_Modules_Options='':Nginx_Modules_Options='--add-module=/usr/local/ngx_http_substitutions_filter_module --add-module=/usr/local/ngx_cache_purge':" lnmp.conf
 ./install.sh lnmp
