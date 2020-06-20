6aa34dfdbe161a77130d9ba5bf62617c76d9279c
zunsking <36225896+zunsking@users.noreply.github.com>
2020-06-20 13:46:46 +0800

    Update lnmp-sub.sh

--- lnmp-sub.sh
+++ lnmp-sub.sh
@@ -2,13 +2,13 @@
 ############### Debian install lnmp and add ngx_http_substitutions_filter_module ###############
 #Author:https://github.com/zunsking
 ####################### END #######################
-apt-get install wget zip unzip -y
+apt-get install wget zip unzip git -y
 cd /usr/local
-wget https://github.com/yaoweibin/ngx_http_substitutions_filter_module/archive/master.zip
-unzip master.zip
+git clone git://github.com/FRiCKLE/ngx_cache_purge.git
+git clone git://github.com/yaoweibin/ngx_http_substitutions_filter_module
 cd
 wget http://soft.vpser.net/lnmp/lnmp1.6.tar.gz -cO lnmp1.6.tar.gz
 tar zxf lnmp1.6.tar.gz
 cd lnmp1.6
-sed -i "s:Nginx_Modules_Options='':Nginx_Modules_Options='--add-module=/usr/local/ngx_http_substitutions_filter_module-master':" lnmp.conf
+sed -i "s:Nginx_Modules_Options='':Nginx_Modules_Options='--add-module=/usr/local/ngx_http_substitutions_filter_module --add-module=/usr/local/ngx_cache_purge':" lnmp.conf
 ./install.sh lnmp
