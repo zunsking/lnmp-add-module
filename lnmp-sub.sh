75d3687eef3b00af9900dae25f14a6dee1cfd79a
zunsking <36225896+zunsking@users.noreply.github.com>
2020-06-23 22:23:07 +0800

    Update lnmp-sub.sh

--- lnmp-sub.sh
+++ lnmp-sub.sh
@@ -6,22 +6,23 @@ apt-get install wget zip unzip git -y
 cd /usr/local
 git clone git://github.com/FRiCKLE/ngx_cache_purge.git
 git clone git://github.com/yaoweibin/ngx_http_substitutions_filter_module
+git clone git://github.com/openresty/headers-more-nginx-module.git
 cd
 wget http://soft.vpser.net/lnmp/lnmp1.7.tar.gz -cO lnmp1.7.tar.gz && tar zxf lnmp1.7.tar.gz
 cd lnmp1.7
-sed -i "s:Nginx_Modules_Options='':Nginx_Modules_Options='--add-module=/usr/local/ngx_http_substitutions_filter_module --add-module=/usr/local/ngx_cache_purge':" lnmp.conf
+sed -i "s:Nginx_Modules_Options='':Nginx_Modules_Options='--add-module=/usr/local/ngx_http_substitutions_filter_module --add-module=/usr/local/ngx_cache_purge --add-module=/usr/local/headers-more-nginx-module':" lnmp.conf
 #./install.sh lnmp
-echo "选择要安装的环境："
+echo "Choose install:"
 echo ""
-echo " 1: 安装完整lnmp"
-echo " 2: 仅安装nginx"
+echo " 1: Insatall full LNMP"
+echo " 2: Only install Nginx"
 echo ""
-read -p "(直接回车取消安装)，输入编号回车继续:" install
+read -p "(Directly Enter to cancel), Enter 1 or 2:" install
 if [[ '1' = "$install" ]]; then
     eval "./install.sh lnmp"
 elif [[ '2' = "$install" ]]; then
     eval "./install.sh nginx"
 else
-    echo "安装取消。"
+    echo "Install canceled."
     exit
 fi
