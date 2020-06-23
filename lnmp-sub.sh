99949c54c1f7fe10e207bac41d435a7b10cb92f1
zunsking <36225896+zunsking@users.noreply.github.com>
2020-06-23 15:22:49 +0800

    Update lnmp-sub.sh

--- lnmp-sub.sh
+++ lnmp-sub.sh
@@ -11,4 +11,18 @@ wget http://soft.vpser.net/lnmp/lnmp1.7.tar.gz -cO lnmp1.7.tar.gz
 tar zxf lnmp1.7.tar.gz
 cd lnmp1.7
 sed -i "s:Nginx_Modules_Options='':Nginx_Modules_Options='--add-module=/usr/local/ngx_http_substitutions_filter_module --add-module=/usr/local/ngx_cache_purge':" lnmp.conf
-./install.sh lnmp
+#./install.sh lnmp
+echo "选择要安装的环境："
+echo ""
+echo " 1: 安装完整lnmp"
+echo " 2: 仅安装nginx"
+echo ""
+read -p "(直接回车取消安装)，输入编号回车继续:" install
+if [[ '1' = "$install" ]]; then
+    eval "./install.sh lnmp"
+elif [[ '2' = "$install" ]]; then
+    eval "./install.sh nginx"
+else
+    echo "安装取消。"
+    exit
+fi
