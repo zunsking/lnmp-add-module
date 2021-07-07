d2c98311f358dbacd1a82e35fb3355a97e3ab4d5
zunsking <36225896+zunsking@users.noreply.github.com>
2021-07-07 13:45:15 +0800

    Create lnmp-sub-centos.sh

---
+++ lnmp-sub-centos.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+############### Debian install lnmp and add ngx_http_substitutions_filter_module ###############
+#Author:https://github.com/zunsking
+####################### END #######################
+yum update && yum upgrade -y
+##
+yum install wget zip unzip git -y
+cd /usr/local
+git clone git://github.com/FRiCKLE/ngx_cache_purge.git
+git clone git://github.com/yaoweibin/ngx_http_substitutions_filter_module
+git clone git://github.com/openresty/headers-more-nginx-module.git
+cd
+wget https://raw.githubusercontent.com/zunsking/lnmp-add-module/master/bakup/backup.sh
+#wget https://raw.githubusercontent.com/zunsking/lnmp-add-module/master/fail2ban.sh
+wget http://soft.vpser.net/lnmp/lnmp1.7.tar.gz -cO lnmp1.7.tar.gz && tar zxf lnmp1.7.tar.gz
+cd lnmp1.7/tools
+sed -i 's#maxretry = 5#maxretry = 2#g' fail2ban.sh
+cd ..
+sed -i "s:Nginx_Modules_Options='':Nginx_Modules_Options='--add-module=/usr/local/ngx_http_substitutions_filter_module --add-module=/usr/local/ngx_cache_purge --add-module=/usr/local/headers-more-nginx-module':" lnmp.conf
+#./install.sh lnmp
+chmod +x *.sh
+echo "Choose install:"
+echo ""
+echo " 1: Install full LNMP"
+echo " 2: Install full LAMP"
+echo " 3: Install full LNMPA"
+echo " 4: Only install Nginx"
+echo " 5: Only install DB"
+echo ""
+read -p "(Directly Enter to cancel), Enter 1 or 2,3,4,5:" install
+if [[ '1' = "$install" ]]; then
+    eval "./install.sh lnmp"
+elif [[ '2' = "$install" ]]; then
+    eval "./install.sh lamp"
+elif [[ '3' = "$install" ]]; then
+    eval "./install.sh lnmpa"
+elif [[ '4' = "$install" ]]; then
+    eval "./install.sh nginx"
+elif [[ '5' = "$install" ]]; then
+    eval "./install.sh db"
+else
+    echo "Install canceled."
+    exit
+fi
+#install fail2ban
+echo "install fail2ban..."
+cd tools
+. fail2ban.sh
+#deny ip:80
+echo "deny ip:80..."
+sed -i "s:server_name _;:server_name _;\n return 444;:" /usr/local/nginx/conf/nginx.conf
+lnmp nginx restart
